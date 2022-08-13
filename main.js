d9027ce939eac57c51679c9e2820caafbef8f289
Quang Nguyen <quangtn001@gmail.com>
2022-08-13 17:16:45 +0700

    update theme

--- main.js
+++ main.js
@@ -112,3 +112,33 @@ function DisplayTodos() {
 
     });
 }
+
+
+// Theme
+let themeToggler = document.querySelector('.theme-toggler');
+
+themeToggler.onclick = () => {
+    themeToggler.classList.toggle('active');
+
+    if (themeToggler.classList.contains('active')) {
+        document.body.classList.add('active');
+    } else {
+        document.body.classList.remove('active');
+    }
+}
+
+document.querySelectorAll('.theme-colors .color').forEach(color => {
+    color.onclick = () => {
+        let background = color.style.background;
+        document.querySelector(':root').style.setProperty('--main-color', background);
+    }
+});
+
+let theme = document.querySelector('.themes-container');
+document.querySelector('#theme-open').onclick = () => {
+    theme.classList.add('open');
+}
+
+document.querySelector('#theme-close').onclick = () => {
+    theme.classList.remove('open');
+}
